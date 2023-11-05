f194f13ed4a0461cbc729107b38490cc7fec47e9
Kristoffer Ödmark <kristoffer.odmark90@gmail.com>
2023-11-05 12:28:20 +0100

    dont need grimblast if using flameshot

--- hyprlandsoftware.sh
+++ hyprlandsoftware.sh
@@ -5,8 +5,8 @@ pacman -S mako  polkit-kde-agent
 # automatic
 pacman -S pipewire wireplumber xdg-desktop-portal-hyprland qt5-wayland qt6-wayland wlroots wl-clipboard
 
-#aur
-yay -S grimblast flameshot-git
+# aur, 
+yay -S flameshot-git
 
 # tools
 pacman -S waybar otf-font-awesome wofi 
