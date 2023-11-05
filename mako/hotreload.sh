0e98e8fad43dc71dfe3ba6602e1e2b58791a1040
Kristoffer Ödmark <kristoffer.odmark90@gmail.com>
2023-11-05 16:11:31 +0100

    modify mako somewhat

---
+++ mako/hotreload.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+CONFIG_FILES="$HOME/.config/mako/config"
+
+trap "killall mako" EXIT
+
+while true; do
+    mako &
+    inotifywait -e create,modify $CONFIG_FILES
+    killall mako
+done
